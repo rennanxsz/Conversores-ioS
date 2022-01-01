3c66f02d8a234a0ed00c4e61eab8231cea0bfc5d
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-01-01 15:35:07 -0300

    Instance components
    Instanciando componentes.

--- Conversores-ioS/Conversores-ioS/ViewController.swift
+++ Conversores-ioS/Conversores-ioS/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tfValue: UITextView!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    
+    @IBOutlet weak var lbResult: UITextView!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    
+    }
+    
 
 }
 

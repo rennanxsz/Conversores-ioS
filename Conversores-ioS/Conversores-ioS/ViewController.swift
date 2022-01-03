174870aa4fe348048db03e12780a943e61642596
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-01-02 21:39:50 -0300

    Create functions
    Criando as funções.

--- Conversores-ioS/Conversores-ioS/ViewController.swift
+++ Conversores-ioS/Conversores-ioS/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tfValue: UITextView!
     @IBOutlet weak var btUnit1: UIButton!
     @IBOutlet weak var btUnit2: UIButton!
-    
+    @IBOutlet weak var lbUnit: UILabel!
     @IBOutlet weak var lbResult: UITextView!
     @IBOutlet weak var lbResultUnit: UILabel!
     
@@ -24,13 +24,69 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+        case "Temperatura":
+            lbUnit.text = "Peso"
+            btUnit1.setTitle("Kilograma", for: .normal)
+            btUnit2.setTitle("Libra", for: .normal)
+        case "Peso":
+            lbUnit.text = "Moeda"
+            btUnit1.setTitle("Real", for: .normal)
+            btUnit2.setTitle("Dollar", for: .normal)
+        case "Moeda":
+            lbUnit.text = "Distancia"
+            btUnit1.setTitle("Metro", for: .normal)
+            btUnit2.setTitle("Milhas", for: .normal)
+        default:
+            lbUnit.text = "Temperatura"
+            btUnit1.setTitle("Kilograma", for: .normal)
+            btUnit2.setTitle("Libra", for: .normal)
+        }
+        
+        convert(nil)
+        
+    }
+    
+    @IBAction func convert(_ sender: UIButton?) {
+        
+        if let sender = sender {
+            if sender == btUnit1{
+                btUnit2.alpha = 0.5
+            }else{
+                btUnit1.alpha = 0.5
+            }
+            sender.alpha = 1.0
+        }
+        
+        switch lbUnit.text! {
+        case "Temperatura":
+            calcTemperature()
+        case "Peso":
+            calcWeight()
+        case "Moeda":
+            calcCurrency()
+        default:
+            calcDistance()
+            
+        }
+        
+        func calcTemperature() {
+            guard let temperature = Double(tfValue.text! else {return} )
+        }
+        
+        func calcWeight() {
+            
+        }
+        
+        func calcCurrency() {
+            
+        }
+        
+        func calcDistance() {
+            
+        }
     
     }
     
-    @IBAction func convert(_ sender: UIButton) {
     
-    }
-    
-
 }
-
